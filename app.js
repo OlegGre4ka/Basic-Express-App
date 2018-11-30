b90bb6530f13d3cc0330385c1d8a2f301c454b99
OlegGre4ka <gre4kae@ukr.net>
2018-11-30 18:31:24 +0300

    working with express-handlebars

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 const app = express();
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.set('view engine','pug');
+app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'}));
+app.set('view engine','hbs');
+// app.set('view engine','pug');
 app.set('views','views');
 
 
@@ -18,7 +22,7 @@ app.use(shopRoutes);
 
 app.use(
   (req,res,next) => {
-    res.status(404).render('404',{pageTitle:'Page Not Found'});
+    res.status(404).render('404',{pageTitle:'Page Not Found', errorCSS:true});
     // res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
 
   });
--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,12 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render('shop',{prods: products, pageTitle:'Shop', path:'/'});
+  res.render('shop',{prods: products,
+     pageTitle:'Shop', 
+     hasProduct: products.length>0,
+     activeShop:true,
+     cardCSS:true,
+     path:'/'});
   this.products = [];
   // console.log('shop.js:',adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
