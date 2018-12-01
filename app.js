b891d0f0bf69c4dd44ce2cb734310ecc1b08ee59
OlegGre4ka <gre4kae@ukr.net>
2018-12-01 10:50:52 +0300

    working with EJS

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'}));
-app.set('view engine','hbs');
+// Express - Handlebars
+// app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'}));
+// app.set('view engine','hbs');
+//Pug
 // app.set('view engine','pug');
+
+// EJS
+app.set('view engine','ejs');
 app.set('views','views');
 
 
