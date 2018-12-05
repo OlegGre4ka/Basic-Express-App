302a8d7c9f79c2c7844a3a212cf4ef208ba4894c
OlegGre4ka <gre4kae@ukr.net>
2018-12-05 17:26:03 +0300

    Section-8:Enhancing the App

--- models/product.js
+++ models/product.js
@@ -5,37 +5,39 @@ const rootDir = require("../util/path");
 const p = path.join(rootDir, "data", "products.json");
 
 const getProductFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else{
-        cb(JSON.parse(fileContent));
-      }
-    });
-  }
-
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  
-  constructor(t) {
-    this.title = t;
+  constructor(title,imageUrl,price,description) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   save() {
     // fs.readFile(p, (err, fileContent) => {
     //   let products = [];
-      // if (!err) {
-      //   products = JSON.parse(fileContent);
-      // }
-     getProductFromFile(products =>{
+    // if (!err) {
+    //   products = JSON.parse(fileContent);
+    // }
+    getProductFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err,'err writeFile');
+        console.log(err, "err writeFile");
       });
     });
-     }
+  }
 
   static fetchAll(cb) {
-     getProductFromFile(cb);
+    getProductFromFile(cb);
   }
+
 };
